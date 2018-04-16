8c423a44b9d354a8fe55abe85d5189199a580250
mateuszzarod <mateusz.zarod@gmail.com>
2018-04-16 12:00:30 +0200

    14. Spring Web - Budowanie REST API 1 cz. I

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,7 +1,11 @@
 package com.crud.tasks.domain;
 
 
+import lombok.Getter;
 
+//nie działa mi ani import ani adnotacja
+@Getter
+@AllArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
