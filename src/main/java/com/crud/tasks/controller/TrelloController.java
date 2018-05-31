1f5fe553288d147388344e856be07854b639ac99
mateuszzarod <mateusz.zarod@gmail.com>
2018-05-31 15:11:41 +0200

    18.2 Zadanie będzie składać się z kilku podpunktów, które uczynią nasz kod lepszym, oraz powtórzy poznane przez Ciebie dzisiaj nauki :)
    Przenieś swój Trello username do application.properties, a następnie utwórz nowe pole korzystając z dodanej właściwości w klasie TrelloClient
    
    Przenieś budowanie adresu URL do prywatnej metody.
    
    Wewnątrz klasy TrelloController występuje iteracja po zwróconych tablicach. Rozbuduj iterację o filtrowanie, pozwalając wyświetlić tylko i wyłącznie takie tablice, które posiadają pola id i name oraz których nazwa zawiera w sobie słowo Kodilla.
    (Wyzwanie - Trudne zadanie) Przebuduj warunek decydujący o tym co należy zwrócić w metodzie getTrelloBoards, wykorzystując klasę Optional, o której wspominaliśmy w module 15.

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -71,35 +71,5 @@ public class TrelloController {
         return trelloClient.createNewCard(trelloCardDto);
     }
 
-/*
-    @RequestMapping(method = RequestMethod.POST,
-    value = "createNewCard")
-*/
-
-/*    public List<TrelloBoardDto> getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        return trelloClient.getTrelloBoards().stream()
-                .filter(t -> t.getName().contains("Kodilla"))
-                .filter(trelloBoardDto -> trelloBoardDto.getName() != null && trelloBoardDto.getId() != null)
-                .collect(Collectors.toList());
-    }*/
-
-
 }
-
-
-        /*(Wyzwanie - Trudne zadanie) Przebuduj warunek decydujący o tym co należy zwrócić w
-        metodzie getTrelloBoards, wykorzystując klasę Optional, o której wspominaliśmy w
-        module 15.
-        https://github.com/mateuszzarod/REST-API/commit/f3ac8ae54ff18788a4d7e9e35d61cdd36f24da79*/
-
-        //http://www.oracle.com/technetwork/articles/java/java8-optional-2175753.html
-        //https://codecouple.pl/2016/03/09/pozbadz-sie-null-pointerow-java-util-optional//*
-        /*trelloBoards.forEach(trelloBoardDto -> System.out.println(
-                trelloBoardDto.getId()
-                        + " "
-                        + trelloBoardDto.getName()));
-        return trelloBoards;*/
 
