6d70bdc9fe48145ead4e22e2c952b3b8b997110c
mateuszzarod <mateusz.zarod@gmail.com>
2018-05-31 15:27:03 +0200

    18.2 To próba trudniejszej wersji z lambdami (Wyzwanie - Trudne zadanie) Przebuduj warunek decydujący o tym co należy zwrócić w metodzie getTrelloBoards, wykorzystując klasę Optional, o której wspominaliśmy w module 15.

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 //-> JSON
@@ -47,9 +48,11 @@ public class TrelloController {
         });
 
         return trelloBoards.stream()
-                .filter(trelloBoardDto -> trelloBoardDto.getName() != null && trelloBoardDto.getId() != null)
+                //.filter(trelloBoardDto -> trelloBoardDto.getName() != null && trelloBoardDto.getId() != null)
+                .filter(trelloBoardDto -> Optional.ofNullable(trelloBoardDto.getName()).orElse("Epty Board Name"))
                 .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
-                .collect(Collectors.toList());
+                .collect(Collectors.toList())
+
     }
 
     public void getTrelloBadges(){
@@ -71,5 +74,37 @@ public class TrelloController {
         return trelloClient.createNewCard(trelloCardDto);
     }
 
+/*
+    @RequestMapping(method = RequestMethod.POST,
+    value = "createNewCard")
+*/
+
+/*    public List<TrelloBoardDto> getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        return trelloClient.getTrelloBoards().stream()
+                .filter(t -> t.getName().contains("Kodilla"))
+                .filter(trelloBoardDto -> trelloBoardDto.getName() != null && trelloBoardDto.getId() != null)
+                .collect(Collectors.toList());
+    }*/
+
+
 }
+
+
+        /*(Wyzwanie - Trudne zadanie)
+
+        Przebuduj warunek decydujący o tym co należy zwrócić w
+        metodzie getTrelloBoards, wykorzystując klasę Optional, o której wspominaliśmy w
+        module 15.
+        https://github.com/mateuszzarod/REST-API/commit/f3ac8ae54ff18788a4d7e9e35d61cdd36f24da79*/
+
+        //http://www.oracle.com/technetwork/articles/java/java8-optional-2175753.html
+        //https://codecouple.pl/2016/03/09/pozbadz-sie-null-pointerow-java-util-optional//*
+        /*trelloBoards.forEach(trelloBoardDto -> System.out.println(
+                trelloBoardDto.getId()
+                        + " "
+                        + trelloBoardDto.getName()));
+        return trelloBoards;*/
 
