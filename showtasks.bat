1aea1be97eebfe83fed034f995c896c270438282
mateuszzarod <mateusz.zarod@gmail.com>
2018-06-19 10:41:01 +0200

    17.5 umieszczanie na Heroku :) Mam problem po zmianie Spring nie startuje aplikacji

---
+++ showtasks.bat
@@ -0,0 +1,22 @@
+call runcrud
+if "%ERRORLEVEL%" == "0" goto runbrowser
+echo.
+echo RUNCRUD has errors - breaking work
+goto fail
+
+:runbrowser
+start chrome.exe http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto stoptomcat
+echo Cannot open the browser.
+goto fail
+
+:stoptomcat
+call %CATALINA_HOME%\bin\shutdown.bat
+
+:fail
+echo.
+echo There were errors.
+
+:end
+echo.
+echo Work is finished.
