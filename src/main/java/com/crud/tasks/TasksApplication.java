5a3d576c86c79d2ddfd9675fcb92b23cbbddd53c
mateuszzarod <mateusz.zarod@gmail.com>
2018-06-20 13:38:10 +0200

    Aplikacja TASK wersja lokalna Aplikacja włącza się. http://localhost:8888/tasks_frontend nie działa

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -10,15 +10,15 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class TasksApplication  /**extends SpringBootServletInitializer**/  {
-	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
-	}
+public class TasksApplication extends SpringBootServletInitializer {
 
-/**
- @Override
- protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
- return application.sources(TasksApplication.class);
- }	**/
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TasksApplication.class);
+    }
 
 }
--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,10 +3,7 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.*;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +17,7 @@ import static java.util.Objects.*;
 @RestController
 
 @RequestMapping("/v1/trello")
+@CrossOrigin(origins = "*")
 public class TrelloController {
 
     @Autowired
